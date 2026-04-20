53744efc8fbec45f75ddef5bbb30caad1e3668de
ir9 <kashira@ir9.jp>
2026-04-21 04:31:46 +0900

    fix metadata

--- mkwsb/Form1.Designer.cs
+++ mkwsb/Form1.Designer.cs
@@ -740,7 +740,7 @@
 			this.Controls.Add(this.buttonSave);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Name = "Form1";
-			this.Text = "Form1";
+			this.Text = "WSB configuration tool - v1.00";
 			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
 			this.Load += new System.EventHandler(this.Form1_Load);
 			this.tableLayoutPanel1.ResumeLayout(false);
